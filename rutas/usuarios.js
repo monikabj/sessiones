62771e731e525c6fc553e0e9e1a7a263844398f8
unknown <monikavh.ti22@utsjr.edu.mx>
2023-08-10 14:41:01 -0500

    correcion2

--- rutas/usuarios.js
+++ rutas/usuarios.js
@@ -46,4 +46,54 @@ ruta.get("/logout",(req,res)=>{
       req.session.destroy();
       res.redirect("/");
     });
-module.exports=ruta;
+    var ruta=require("express").Router();
+
+
+    ruta.get("/",(req, res)=>{
+    res.render("inicio");
+    })
+    
+    ruta.get("/login",(req,res)=>{
+        res.render("login");
+    })
+    
+    ruta.post("/revisar",(req,res)=>{
+        console.log(req.body.usuario);
+        console.log((req.body.password));
+        
+        
+        if(req.body.usuario=="abc" && req.body.pass=="123"){
+            req.session.usuario=req.body.usuario
+            res.redirect("/bienvenido");
+        }else{
+            res.redirect("/error");
+        }
+    });
+    
+    ruta.get("/bienvenido",(req,res)=>{
+        if(req.session.usuario){
+            res.render("bienvenido");
+        }else{
+            res.redirect("/error");
+        }
+    
+    }) 
+    
+    ruta.get("/error",(req,res)=>{
+        res.render("error");
+        })
+    
+    ruta.get("/protegido",(req,res)=>{
+        if(req.session.usuario){
+            res.render("bienvenido");
+        }else{
+            res.redirect("/error");
+        }
+    });
+    
+    ruta.get("/logout", (req,res)=>{
+        req.session.destroy();
+        res.redirect("/")
+    })
+    
+    module.exports=ruta;
